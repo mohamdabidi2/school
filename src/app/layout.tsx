38c67b89823911a1b377eac8318ba06a26720c2a
mohamdabidi2 <mohamedabidi58@gmail.com>
2025-11-26 23:29:11 +0100

    44

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
-      fallbackRedirectUrl="/"
+      signInFallbackRedirectUrl="/"
+      signUpFallbackRedirectUrl="/"
     >
       <html lang="fr">
         <head>
