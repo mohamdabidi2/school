bb75965689b37c6f9c041cab13c2d6b1eb847322
mohamdabidi2 <mohamedabidi58@gmail.com>
2025-11-26 18:24:23 +0100

    v23

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -16,7 +16,7 @@ const PageUniqueEnseignant = async ({
 }: {
   params: { id: string };
 }) => {
-  const { sessionClaims } = auth();
+  const { sessionClaims } = await auth();
   const role = (sessionClaims?.metadata as { role?: string })?.role;
 
   const enseignant:
