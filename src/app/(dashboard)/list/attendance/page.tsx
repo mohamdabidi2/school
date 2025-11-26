95a7b93355e1ea20fc6aef7536a8f77d61f0ac8b
mohamdabidi2 <mohamedabidi58@gmail.com>
2025-11-26 18:06:24 +0100

    v17

--- src/app/(dashboard)/list/attendance/page.tsx
+++ src/app/(dashboard)/list/attendance/page.tsx
@@ -14,7 +14,7 @@ const AttendanceListPage = async ({
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
-  const { userId, sessionClaims } = auth();
+  const { userId, sessionClaims } = await auth();
   const role = (sessionClaims?.metadata as { role?: string })?.role;
 
   const { page, ...queryParams } = searchParams;
